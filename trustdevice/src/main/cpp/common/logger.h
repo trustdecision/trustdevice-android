56ea55e714bfdf90d2ee3bb88cea6c325cd6273c
collam <1050078134@qq.com>
2023-06-21 11:18:44 +0800

    Add logger.h

---
+++ trustdevice/src/main/cpp/common/logger.h
@@ -0,0 +1,17 @@
+//
+// Created by collam on 2023/6/21.
+//
+
+#ifndef TRUSTDEVICE_ANDROID_LOGGER_H
+#define TRUSTDEVICE_ANDROID_LOGGER_H
+
+#include <android/log.h>
+
+#define TAG "TrustDevice"
+
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+#define LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#define LOGW(...)  __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+
+#endif //TRUSTDEVICE_ANDROID_LOGGER_H
